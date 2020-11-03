62f8c3e955acbdf56e7a933768ea8600cceb2819
quantum-mob <quantum.mob21@gmail.com>
2020-11-03 21:05:30 +0900

    Public release

--- Q3/PacletInfo.m
+++ Q3/PacletInfo.m
@@ -4,7 +4,7 @@
 
 Paclet[
     Name -> "Q3",
-    Version -> "1.0.2",
+    Version -> "1.0.3",
     MathematicaVersion -> "6+",
     Description -> "Mathematica application to help study quantum information processing, quantum many-body systems, and quantum spin systems.",
     Creator -> "Mahn-Soo Choi (Korea University)",
